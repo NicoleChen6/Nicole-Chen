2b3e59812dc2efc9b169ab6ee2fc2f1f8b0a7202
NicoleChen6 <30436225+NicoleChen6@users.noreply.github.com>
2017-08-01 21:46:09 +1000

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,5 @@
+# A Comment, this is so you can read your program later
+# Anything afer the # is ignored by Python
+
+print "I could have code like this." # and the comment after is ignored
+print " This will run"
---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name = 'Nicole Chen'
+my_age = 22 # I look even younger
+my_height = 155 # cm
+my_weight = 44 # kg
+my_eyes = 'black'
+my_teeth = 'white'
+my_hair = 'black'
+
+print "let's talk about %s." % my_name
+print "She's %d cm tall." % my_height
+print "She's %d kg heavy" % my_weight
+print "Actually that's a little thin"
+print "She got %s eyes and %s hair." % (my_eyes, my_hair)
+print "Her teeth are always %s." % my_teeth
+
